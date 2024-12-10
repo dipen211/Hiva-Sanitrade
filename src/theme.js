05705dbeefee75f21336da7f42c39882a57369bf
dipen.patel <dipen.patel@covrtech.com>
2024-12-10 18:01:50 +0530

    Added theme

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',  // Blue
+    },
+    secondary: {
+      main: '#f50057',  // Pink
+    },
+  },
+});
